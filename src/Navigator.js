2ce3133706968a696013b3a760b7f78f67c6da7c
Carlos Martins <cmartins.dev@gmail.com>
2020-05-23 18:43:56 -0300

    Added the Drawer Navigator and applied changes in the Taskslist to receive news perspectives (today, tomorrow, week & month)

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,8 +1,43 @@
+import React from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createDrawerNavigator } from 'react-navigation-drawer'
 
 // importando as telas
 import Auth from './screens/Auth'
 import TaskList from './screens/TaskList'
+
+const menuRoutes = {
+  Today: {
+    name: 'Today',
+    screen: props => <TaskList title='Hoje' daysAhead={0} {...props} />,
+    navigationOptions: {
+      title: 'Hoje'
+    }
+  },
+  Tomorrow: {
+    name: 'Tomorrow',
+    screen: props => <TaskList title='Amanhã' daysAhead={1} {...props} />,
+    navigationOptions: {
+      title: 'Amanhã'
+    }
+  },
+  Week: {
+    name: 'Week',
+    screen: props => <TaskList title='Semana' daysAhead={7} {...props} />,
+    navigationOptions: {
+      title: 'Semana'
+    }
+  },
+  Month: {
+    name: 'Month',
+    screen: props => <TaskList title='Mês' daysAhead={30} {...props} />,
+    navigationOptions: {
+      title: 'Mês'
+    }
+  }
+}
+
+const menuNavigator = createDrawerNavigator(menuRoutes)
 
 // Routas
 const mainRoutes = {
@@ -12,7 +47,7 @@ const mainRoutes = {
   },
   Home: {
     name: 'Home',
-    screen: TaskList
+    screen: menuNavigator
   }
 }
 
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,23 +1,24 @@
 import React, {Component} from "react"
 import { View, Text, StyleSheet, ImageBackground, FlatList, TouchableOpacity, Platform, Alert } from "react-native"
-// Importando stylos
-import commonStyles from '../commonStyles'
-// importar imagem
-import todayImage from '../../assets/imgs/today.jpg'
+import AsyncStorage from '@react-native-community/async-storage'
+import axios from "axios"
+import Task from '../components/Task'
+import AddTask from './AddTask'
+import { server, showError, showSuccess } from '../common';
+
 // Traduzir o valor das datas para PT-BR
 import moment from 'moment'
 import 'moment/locale/pt-br'
-
-// Importando os Components
-import Task from '../components/Task'
-import AddTask from './AddTask'
-
+// Importando stylos
+import commonStyles from '../commonStyles'
 // importando os Icones
 import Icon from 'react-native-vector-icons/FontAwesome'
 
-import AsyncStorage from '@react-native-community/async-storage'
-import { server, showError, showSuccess } from '../common';
-import axios from "axios"
+// importar imagem
+import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
 
 const initState = { showDoneTasks: true, showAddTask: false, visibleTasks: [], tasks:[] }
 
@@ -25,6 +26,37 @@ export default class TaskList extends Component {
   // estado
   state = { ...initState }
   
+  // função para trocar as imagens de acordo com as perspectivas.
+  getImage = () => {
+    switch (this.props.daysAhead) {
+      case 0:
+        return todayImage
+        break;
+      case 1:
+        return tomorrowImage
+        break;
+      case 7:
+        return weekImage
+        break;
+      default:
+        return monthImage
+        break;
+    }
+  }
+  // função para trocar a cor do botão add Task de acordo com a perspectiva selecionada.
+  getColor = () => {
+    switch (this.props.daysAhead) {
+      case 0:
+        return commonStyles.colors.today 
+      case 1:
+        return commonStyles.colors.tomorrow
+      case 7:
+        return commonStyles.colors.week
+      default:
+        return commonStyles.colors.month;
+    }
+  }
+
   // função ciclo de vida
   componentDidMount = async () => {
     const stateString = await AsyncStorage.getItem('TasksState')
@@ -32,28 +64,14 @@ export default class TaskList extends Component {
     this.setState({ showDoneTasks: savedState.showDoneTasks }, this.filterTasks)
     this.loadTasks()
   }
-
- // carregar as tasks do banco de dados 
-  loadTasks = async () => {
-    try {
-      const maxDate = moment().format('YYYY-MM-DD 23:59:59')
-      const res = await axios.get(`${server}/tasks?date=${maxDate}`)
-      this.setState({tasks: res.data}, this.filterTasks)
-    } catch (e) {
-      showError(e)
-    }
-  }
-
-  // função para fazer a alternancia para ver as tasks concluidas.
-  toggleFilter = () => {
-    this.setState({showDoneTasks: !this.state.showDoneTasks}, this.filterTasks)
-  }
-
   // Função para trazer todas as tasks pendentes
   isPending = task => {
     return task.doneAt === null
   }
-
+  // função para fazer a alternancia para ver as tasks concluidas.
+  toggleFilter = () => {
+    this.setState({showDoneTasks: !this.state.showDoneTasks}, this.filterTasks)
+  }  
   // filtrar as tasks
   filterTasks  = () => {
     let visibleTasks = null
@@ -67,7 +85,21 @@ export default class TaskList extends Component {
       showDoneTasks: this.state.showDoneTasks
     }))
   }
-
+  
+  /**********************************************
+   Author: Carlos Martins
+   FUNÇOES QUE EXECUTAM ALGUMA TAREFA NO BACKEND
+  ***********************************************/
+  // carregar as tasks do banco de dados 
+  loadTasks = async () => {
+    try {
+      const maxDate = moment().add({days: this.props.daysAhead}).format('YYYY-MM-DD 23:59:59')
+      const res = await axios.get(`${server}/tasks?date=${maxDate}`)
+      this.setState({tasks: res.data}, this.filterTasks)
+    } catch (e) {
+      showError(e)
+    }
+  }
   // marcar e desmarcar a task como concluido
   toggleTask = async taskId => {
     try {
@@ -87,7 +119,6 @@ export default class TaskList extends Component {
     // // e selecionar a tarefa como concluida ele deve esconde-la.
     // this.setState({tasks: tasks}, this.filterTasks)
   }
-
   // função para add nova task
   addTask = async newTask => {
     try {
@@ -101,11 +132,10 @@ export default class TaskList extends Component {
       showError(e)
     }
   }
-
   // Remover a task
-  deleteTask = async id => {
+  deleteTask = async taskid => {
     try {
-      await axios.delete(`${server}/tasks/${id}`)
+      await axios.delete(`${server}/tasks/${taskid}`)
       this.loadTasks()
     } catch (e) {
       showError(e)
@@ -123,8 +153,12 @@ export default class TaskList extends Component {
                   onSave={this.addTask} />
 
         <ImageBackground style={styles.background} // Header com a imagem 
-                         source={todayImage} >
+                         source={this.getImage()} >
           <View style={styles.iconBar}>
+            <TouchableOpacity onPress={() => this.props.navigation.openDrawer() }>
+              <Icon  name='bars' size={25} color={commonStyles.colors.secondary} />
+            </TouchableOpacity>
+
             <TouchableOpacity onPress={this.toggleFilter}>
               <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash' } // Icone
                     size={25}
@@ -133,7 +167,7 @@ export default class TaskList extends Component {
           </View>
 
           <View style={styles.titleBar}>
-            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.title}>{this.props.title}</Text>
             <Text style={styles.subtitle}>{today}</Text>
           </View>
         </ImageBackground>
@@ -145,7 +179,7 @@ export default class TaskList extends Component {
         </View>
 
         
-        <TouchableOpacity style={styles.addButton} // Botão para adicionar uma nova tarefa
+        <TouchableOpacity style={[styles.addButton, { backgroundColor: this.getColor() }]} // Botão para adicionar uma nova tarefa
                           activeOpacity={0.7} // mexendo na opacidade quando clicado
                           onPress={() => this.setState({showAddTask: true})} >
           <Icon name="plus" size={25} color={commonStyles.colors.secondary} /> 
@@ -187,7 +221,7 @@ const styles = StyleSheet.create({
   iconBar: {
     flexDirection: 'row',
     marginHorizontal: 20,
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
     // alterando a margin de acordo com a plataforma.
     marginTop: Platform.OS === 'ios' ? 40 : 10
   },
@@ -198,7 +232,6 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 25,
-    backgroundColor: commonStyles.colors.today,
     justifyContent: 'center',
     alignItems: 'center',
   }
